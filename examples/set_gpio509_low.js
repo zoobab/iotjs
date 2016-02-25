358d3176d0befcca41d32d75e6f0c53a2cf52962
Benjamin Henrion <zoobab@gmail.com>
2016-02-25 16:09:31 +0100

    add some examples: set_gpio509_high.js + set_gpio509_low.js

---
+++ examples/set_gpio509_low.js
@@ -0,0 +1,10 @@
+var gpio = require('gpio');
+
+gpio.initialize();
+
+gpio.on('initialize', function() {
+  console.log("Setting GPIO_509=OUT");
+  gpio.setPin(509, "out");
+  console.log("Setting GPIO_509=LOW");
+  gpio.writePin(509, 0);
+});
