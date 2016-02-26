ef8f6365218b5e38db87acef1d1ef0536c319ec8
Benjamin Henrion <zoobab@gmail.com>
2016-02-26 16:47:15 +0100

    add iotjs_js.h

--- examples/set_gpio509_high.js
+++ examples/set_gpio509_high.js
@@ -3,7 +3,7 @@ var gpio = require('gpio');
 gpio.initialize();
 
 gpio.on('initialize', function() {
-  console.log("Setting GPIO_509=OUT");
+//  console.log("Setting GPIO_509=OUT");
   gpio.setPin(509, "out");
   console.log("Setting GPIO_509=HIGH");
   gpio.writePin(509, 1);
--- examples/set_gpio509_low.js
+++ examples/set_gpio509_low.js
@@ -3,7 +3,7 @@ var gpio = require('gpio');
 gpio.initialize();
 
 gpio.on('initialize', function() {
-  console.log("Setting GPIO_509=OUT");
+//  console.log("Setting GPIO_509=OUT");
   gpio.setPin(509, "out");
   console.log("Setting GPIO_509=LOW");
   gpio.writePin(509, 0);
